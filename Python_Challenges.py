2ba74a56be4c4f29f8aa6092187fe272d0a60322
aacr-git <angelcalcano.r@hotmail.com>
2025-10-22 20:31:49 -0400

    Create Python_Challenges.py

---
+++ Python_Challenges.py
@@ -0,0 +1,33 @@
+#¿Está en Equilibrio la Alianza entre Reed Richards y Johnny Storm?
+
+#En el universo de los 4 Fantásticos, la unión y el equilibrio entre los poderes es fundamental para enfrentar cualquier desafío. En este problema, nos centraremos en dos de sus miembros:
+
+#Reed Richards (Mr. Fantastic), representado por la letra R.
+#Johnny Storm (La Antorcha Humana), representado por la letra J.
+
+#Objetivo:
+
+#Crea una función en Python que reciba una cadena de texto. Esta función debe contar cuántas veces aparece la letra R (para Reed Richards) y cuántas veces aparece la letra J (para Johnny Storm) en la cadena.
+
+#Si la cantidad de R y la cantidad de J son iguales, se considera que la alianza entre la mente y el fuego está en equilibrio y la función debe retornar True.
+#Si las cantidades no son iguales, la función debe retornar False.
+#En el caso de que no aparezca ninguna de las dos letras en la cadena, se entiende que el equilibrio se mantiene (0 = 0), por lo que la función debe retornar True.
+
+
+
+chain_text = input("enter some large string text: ")
+
+def reed_johnny_alliance (reedvsjohnny):
+    if reedvsjohnny.count('r') != reedvsjohnny.count('j'):
+        print(False)
+        
+    elif reedvsjohnny.count('r') == reedvsjohnny.count('j') and reedvsjohnny.count('r') != 0 :
+        print(True)
+        
+    elif reedvsjohnny.count('r') == 0 and reedvsjohnny.count('j') == 0:
+        print(True)
+        
+        
+
+    
+reed_johnny_alliance(chain_text)
